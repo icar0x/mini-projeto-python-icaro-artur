cf0fcf533416a1012352cc13a0e140bc42dee07c
Icaro Ryan <icaro0721j@gmail.com>
2025-10-30 20:27:38 -0300

    feat: Implementa a estrutura do menu interativo

--- estoque_app.py
+++ estoque_app.py
@@ -2,10 +2,10 @@ titulo = "Menu"
 print("*" * 30)
 print(titulo.upper().center(30))
 print("*" * 30)
-print("1 - Cadastrar produto")
-print("2 - Listar produtos")
-print("3 - Atualizar produto")
-print("4 - Excluir produto")
-print("0 - Sair")
-
-opcao = ("Escolha uma opção: ").strip().lower()
+print("""
+1 - Cadastrar produto
+2 - Listar produtos
+3 - Atualizar produto
+4 - Excluir produto
+0 - Sair
+""")
