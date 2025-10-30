3d96c7e33f9fee92edcfffbb7e81934fc71a1762
Icaro Ryan <icaro0721j@gmail.com>
2025-10-30 20:19:18 -0300

    adicionei o menu

--- estoque_app.py
+++ estoque_app.py
@@ -2,3 +2,10 @@ titulo = "Menu"
 print("*" * 30)
 print(titulo.upper().center(30))
 print("*" * 30)
+print("1 - Cadastrar produto")
+print("2 - Listar produtos")
+print("3 - Atualizar produto")
+print("4 - Excluir produto")
+print("0 - Sair")
+
+opcao = ("Escolha uma opção: ").strip().lower()
